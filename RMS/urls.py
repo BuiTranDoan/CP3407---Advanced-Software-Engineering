9fd75c7e3ac6e0fbc7dc9fd8e01a2fbe865ca82d
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-07-10 19:12:18 +0800

    Category pages

--- RMS/urls.py
+++ RMS/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.views import home
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('menu/', include('menu.urls')),
     path('order/', include('order.urls')),
     path('inventory/', include('inventory.urls')),
+
 ]
--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, CustomizationViewSet, MenuItemViewSet
+from .views import *
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
@@ -8,5 +8,12 @@ router.register(r'customizations', CustomizationViewSet)
 router.register(r'menu-items', MenuItemViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('', menu, name='menu'),
+    path('category/', category, name='category'),
+    path('category/add/', category_add, name='category_add'),
+    path('category/<int:pk>/', category_edit, name='category_edit'),
+    path('category/delete/<int:pk>/', category_delete, name='category_delete'),
+    path('menu/', menu_list_view, name='menu-list'),
+    path('customization/', customization, name='customization'),
 ]
